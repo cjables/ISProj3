e1dde66f08be5501f35ee0942a85ae407729a3c9
Brian Foster <brianisbrilliant@gmail.com>
2022-04-06 13:24:00 -0500

    added textmesh pro, started building state machine

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -9,26 +9,18 @@ public class Patrol : MonoBehaviour {
 
     public Transform[] points;
     public float reactionTime = 1;      // how long can we see the player before springing into action.
-    public List<Transform> looks = new List<Transform>();
-    public Transform eyePivot;
-    public AnimationCurve curve;
+
+    public enum state {Patrolling, Chasing, Searching};
+    public state currentState = state.Patrolling;
 
     private int destPoint = 0;
     private NavMeshAgent agent;
 
-    bool stopped = false;
-    bool hunting = false;
-
-    IEnumerator stopAndLook;
-
     private AIFoV fov;
-
 
     void Start () {
         agent = GetComponent<NavMeshAgent>();
         fov = GetComponent<AIFoV>();
-
-        stopAndLook = WaitAtPatrolPoint();
 
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
@@ -54,18 +46,12 @@ public class Patrol : MonoBehaviour {
 
     private float eyesOnPlayerTimer = 0;
 
-    void Update () {
+    void Patrolling() {
         if(fov.canSeePlayer == true) {
             eyesOnPlayerTimer += Time.deltaTime;
-            // Debug.Log("EyesOnPlayerTimer: " + eyesOnPlayerTimer);
             if(eyesOnPlayerTimer > reactionTime) {
-                hunting = true;
-                if(stopped) {
-                    StopCoroutine(stopAndLook);
-                    stopped = false;
-                    eyePivot.rotation = looks[0].rotation;
-                }
-                // return;     // don't look at anything else in the Update function.
+                currentState = state.Chasing;
+                return;     // don't look at anything else in the Update function.
             }
         }
         else {
@@ -74,62 +60,35 @@ public class Patrol : MonoBehaviour {
             eyesOnPlayerTimer = 0;
         }
 
-        if(hunting) {
-            agent.destination = fov.player.position;
-            float distance = Vector3.Distance(transform.position, fov.player.position);
-            Debug.Log("Distance: " + distance);
-            if(distance > 20) {
-                hunting = false;
-                StartCoroutine(stopAndLook);
-            }
-        }
-
         // Choose the next destination point when the agent gets
         // close to the current one.
         if (!agent.pathPending && agent.remainingDistance < 0.5f) {
-            if(hunting) return;     // hunt the player forever.
-            if(!stopped)StartCoroutine(stopAndLook);
+            StartCoroutine(WaitAtPatrolPoint());        // this is searching
+                                                        // go to the searching state.
         }
     }
 
-    IEnumerator WaitAtPatrolPoint() {
-        Debug.Log("Starting WaitAtPatrolPoint()");
-        stopped = true;
-        // play the waiting animation
-        // stop
-        // Debug.Log("Stopping.");
-        // agent.destination = this.transform.position;
-        // look left and right
-        // lerp -90 degrees and wait - or rotate
-        float timer = 0f;
-        while(timer < 1) {
-            eyePivot.rotation = Quaternion.Lerp(looks[0].rotation, looks[1].rotation, curve.Evaluate(timer));
-            timer += Time.deltaTime;
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(1);
-
-        // lerp 180 degrees and wait
-        timer = 0f;
-        while(timer < 1) {
-            eyePivot.rotation = Quaternion.Lerp(looks[1].rotation, looks[2].rotation, curve.Evaluate(timer));
-            timer += Time.deltaTime;
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(1);
-
-        // lerp -90 degrees and go to next point.
-        timer = 0f;
-        while(timer < 1) {
-            eyePivot.rotation = Quaternion.Lerp(looks[2].rotation, looks[0].rotation, curve.Evaluate(timer));
-            timer += Time.deltaTime;
-            yield return null;
-        }
+    void Chasing() {
+        agent.destination = fov.player.position;
+    }
+    
+    void Searching() {
         
-        Debug.Log("Going to Next Point.");
-        stopped = false;
+    }
+
+    void Update () {
+        switch(currentState) {
+            case state.Patrolling: Patrolling(); break;
+            case state.Chasing: Chasing(); break;
+            case state.Searching: Searching(); break;
+        }
+
+    }
+
+
+
+    IEnumerator WaitAtPatrolPoint() {       
+        yield return new WaitForSeconds(2);
         GotoNextPoint();
     }
 }
